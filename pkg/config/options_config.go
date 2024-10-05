3ab97182b9202e011f2efe28abb0daf687f090e6
cezar-r <cez.rata1@gmail.com>
2024-10-05 12:52:12 -0700

    chore: cleanup chart config options
    Signed-off-by: cezar-r <cez.rata1@gmail.com>

--- pkg/config/options_config.go
+++ pkg/config/options_config.go
@@ -8,8 +8,8 @@ import (
 
 func OptionsConfig() {
 	fmt.Println("\nConfig options:")
-	fmt.Println("up_color:   red green yellow purple magenta cyan white gray black")
-	fmt.Println("down_color: red green yellow purple magenta cyan white gray black")
+	fmt.Println("up_color:   [red green yellow purple magenta cyan white gray black]")
+	fmt.Println("down_color: [red green yellow purple magenta cyan white gray black]")
 	fmt.Printf("default_tf: %s\n", c.Intervals)
 	fmt.Println("equity_wl:  any shorthand equity ticker (AAPL, TSLA, etc) available on Yahoo Finance")
 	fmt.Println("crypto_wl:  any shorthand crypto ticker (BTC, ETH, etc) available on Coinbase")
